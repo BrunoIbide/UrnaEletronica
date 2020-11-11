b59311ca15e7f9f15c9fcde57b3c40f5d1ae83ab
BrunoIbide <brunoibide@hotmail.com>
2020-11-11 14:06:03 -0300

    Terceiro Commit

---
+++ UrnaEletronica/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa
+{
+    String nome;
+    String rg;
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setRg(String nome){
+        this.rg = rg;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+     public String getRg(){
+        return this.rg;
+    }
+    
+    
+}
